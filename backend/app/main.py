aa24c81868035cb671a29aa7016d56dee6725b1e
gongbu22 <pyujin0711@naver.com>
2026-06-10 00:20:13 +0900

    frontend init

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import chat
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 # chat router 연결
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
